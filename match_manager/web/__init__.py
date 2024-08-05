e2de32294c33ad0db1bbc2a5af9cea61160c26e4
limette <limette>
2024-08-05 22:16:19 +0200

    configure example react app

--- match_manager/web/__init__.py
+++ match_manager/web/__init__.py
@@ -2,13 +2,16 @@
 
 import os
 
-from quart import Quart, url_for
+from quart import Quart, url_for, render_template
 
 
 from .. import config
 from .api import login
 
-app = Quart(__name__)
+# serve the react application, built with `npm run build` in `client/`, as static files
+app = Quart(__name__, static_url_path='', static_folder='client/build')
+
+# configure secrets for oauth2 -- login with discord
 app.secret_key = config.webserver.secret
 app.config['DISCORD_CLIENT_ID'] = config.discord.client_id
 app.config['DISCORD_CLIENT_SECRET'] = config.discord.client_secret
@@ -16,12 +19,14 @@ app.config['DISCORD_BOT_TOKEN'] = config.discord.bot_token
 app.config['DISCORD_REDIRECT_URI'] = \
     f'http://{config.webserver.host}:{config.webserver.port}/discord-oauth-callback'
 
+# for local development, allow insecure transport (no https configured)
 if config.webserver.host == 'localhost':
     os.environ['OAUTHLIB_INSECURE_TRANSPORT'] = '1'
 
+# register the different routes from their blueprints
 app.register_blueprint(login.blue)
 
 @app.route('/')
 async def index():
     """the initial landing page"""
-    return f'hello. please login: <br> <a href="{url_for("login.login")}">with discord</a>'
+    return await app.send_static_file('index.html')
