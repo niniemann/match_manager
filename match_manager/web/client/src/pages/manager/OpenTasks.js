340ac8f2b879818b7cc45ec9f0c674ee608290b5
niniemann <>
2025-03-27 21:30:16 +0100

    manager: reject/suggest match time

--- match_manager/web/client/src/pages/manager/OpenTasks.js
+++ match_manager/web/client/src/pages/manager/OpenTasks.js
@@ -3,12 +3,14 @@ import {
   Box,
   Button,
   ButtonGroup,
+  DatePicker,
   Header,
   KeyValuePairs,
   Modal,
   SpaceBetween,
   StatusIndicator,
   Table,
+  TimeInput,
 } from "@cloudscape-design/components";
 import { useTeamLookup } from "../../hooks/useTeams";
 import { useParams } from "react-router-dom";
@@ -19,10 +21,29 @@ import { toast } from "react-toastify";
 import { Avatar, LoadingBar } from "@cloudscape-design/chat-components";
 import { showErrorToast } from "../../components/ErrorToast";
 
-function MatchTimeConfirmationModal({ team_id, match, onClose, onConfirm }) {
+function MatchTimeConfirmationModal({ team_id, match, onClose }) {
   const { data: teams } = useTeamLookup();
   const opponent_id = team_id === match.team_a ? match.team_b : match.team_a;
-  const opponent = teams[opponent_id];
+  const opponent = teams && teams[opponent_id];
+
+  const { mutate: suggestMatchTime } = useSuggestMatchTime();
+
+  const onConfirm = () => {
+    suggestMatchTime(
+      {
+        suggesting_team: team_id,
+        match_id: match.id,
+        match_time: match.match_time,
+      },
+      {
+        onSuccess: () => {
+          toast.success("Confirmed match time/date.");
+          onClose();
+        },
+        onError: showErrorToast,
+      }
+    );
+  };
 
   return (
     <Modal
@@ -34,7 +55,7 @@ function MatchTimeConfirmationModal({ team_id, match, onClose, onConfirm }) {
             <Button onClick={onClose} variant="link">
               Cancel
             </Button>
-            <Button variant="primary" onClick={onConfirm}>
+            <Button variant="primary" onClick={onConfirm} disabled={!match}>
               Confirm
             </Button>
           </SpaceBetween>
@@ -52,12 +73,143 @@ function MatchTimeConfirmationModal({ team_id, match, onClose, onConfirm }) {
               label: "Opponent",
               value: (
                 <SpaceBetween direction="horizontal" size="m">
-                  <Avatar imgUrl={opponent.logo_url} />
-                  {opponent.name}
+                  <Avatar imgUrl={opponent?.logo_url} />
+                  {opponent?.name}
                 </SpaceBetween>
               ),
             },
             { label: "Date/Time", value: <DateTimeDisplay timestamp={match.match_time} /> },
+          ]}
+        />
+      </SpaceBetween>
+    </Modal>
+  );
+}
+
+/**
+ * @param {Object} param0
+ * @param {*} param0.team_id
+ * @param {*} param0.match
+ * @param {'suggest' | 'reject'} param0.reason
+ * @param {*} param0.onClose
+ */
+function MatchTimeSuggestionModal({ team_id, match, reason, onClose }) {
+  const { data: teams } = useTeamLookup();
+  const opponent_id = team_id === match.team_a ? match.team_b : match.team_a;
+  const opponent = teams && teams[opponent_id];
+
+  const old_dt = new Date(match.match_time);
+  const [suggestion, setSuggestion] = useState(
+    reason === "suggest"
+      ? { date: undefined, time: undefined, datetime: undefined }
+      : {
+          date: `${old_dt.getUTCFullYear()}-${String(old_dt.getUTCMonth() + 1).padStart(2, "0")}-${String(
+            old_dt.getUTCDate()
+          ).padStart(2, "0")}`,
+          time: `${String(old_dt.getUTCHours()).padStart(2, "0")}:${String(old_dt.getUTCMinutes()).padStart(2, "0")}`,
+          datetime: old_dt,
+        }
+  );
+
+  const { mutate: suggestMatchTime } = useSuggestMatchTime();
+
+  const handleDateChange = (date) => {
+    setSuggestion((old) => ({
+      ...old,
+      date: date,
+      datetime: (date && old.time && new Date(`${date}T${old.time}:00Z`)) || undefined,
+    }));
+  };
+
+  const handleTimeChange = (time) => {
+    setSuggestion((old) => ({
+      ...old,
+      time: time,
+      datetime: (time && old.date && new Date(`${old.date}T${time}:00Z`)) || undefined,
+    }));
+  };
+
+  const onConfirm = () => {
+    // Note: There is no difference between suggesting and rejecting a date/time. Both are done by sending a suggestion.
+    suggestMatchTime(
+      { match_id: match.id, suggesting_team: team_id, match_time: suggestion.datetime },
+      {
+        onSuccess: () => {
+          toast.success(`Sent suggestion for match #${match.id}`);
+          onClose();
+        },
+        onError: showErrorToast,
+      }
+    );
+  };
+
+  return (
+    <Modal
+      visible={true}
+      header={
+        <Header>
+          <span style={{ textTransform: "capitalize" }}>{reason}</span> date & time for match #{match.id}
+        </Header>
+      }
+      footer={
+        <Box float="right">
+          <SpaceBetween direction="horizontal" size="xs">
+            <Button onClick={onClose} variant="link">
+              Cancel
+            </Button>
+            <Button
+              variant="primary"
+              onClick={onConfirm}
+              disabled={isNaN(suggestion.datetime) || suggestion.datetime?.getTime() == old_dt.getTime()}
+            >
+              {reason === "suggest" ? "Suggest" : "Submit Alternative"}
+            </Button>
+          </SpaceBetween>
+        </Box>
+      }
+      onDismiss={onClose}
+    >
+      <SpaceBetween direction="vertical" size="m">
+        {reason === "reject" && <Alert type="info">To reject the date/time, please suggest an alternative.</Alert>}
+        <KeyValuePairs
+          columns={2}
+          items={[
+            {
+              label: "Opponent",
+              value: (
+                <SpaceBetween direction="horizontal" size="m">
+                  <Avatar imgUrl={opponent?.logo_url} />
+                  <SpaceBetween direction="vertical">
+                    {opponent?.name}
+                    {reason === "reject" && (
+                      <StatusIndicator type="info">
+                        <DateTimeDisplay timestamp={match.match_time} />
+                      </StatusIndicator>
+                    )}
+                  </SpaceBetween>
+                </SpaceBetween>
+              ),
+            },
+            {
+              label: "Date/Time (UTC)",
+              value: (
+                <SpaceBetween direction="vertical" size="s">
+                  <DatePicker onChange={({ detail }) => handleDateChange(detail.value)} value={suggestion.date} />
+                  <TimeInput
+                    onChange={({ detail }) => handleTimeChange(detail.value)}
+                    value={suggestion.time}
+                    format="hh:mm"
+                    placeholder="hh:mm"
+                    invalid={!suggestion.datetime}
+                  />
+                  {!isNaN(suggestion.datetime) && (
+                    <StatusIndicator type="info">
+                      <DateTimeDisplay timestamp={suggestion.datetime} />
+                    </StatusIndicator>
+                  )}
+                </SpaceBetween>
+              ),
+            },
           ]}
         />
       </SpaceBetween>
@@ -73,7 +225,7 @@ function MatchTimeConfirmationModal({ team_id, match, onClose, onConfirm }) {
 function InPlanningTable({ team: team_id, matches }) {
   const { data: teams } = useTeamLookup();
   const [matchToConfirmDateTime, setMatchToConfirmDateTime] = useState(undefined);
-  const { mutate: suggestMatchTime } = useSuggestMatchTime();
+  const [matchToSuggestDateTime, setMatchToSuggestDateTime] = useState(undefined);
 
   // --- helpers to deal with different scheduling states ---
   const set_by_admin = (match) => match.match_time_state === "FIXED" && match.match_time;
@@ -163,6 +315,12 @@ function InPlanningTable({ team: team_id, matches }) {
             case "confirm":
               setMatchToConfirmDateTime(match);
               break;
+            case "suggest":
+              setMatchToSuggestDateTime({ match, reason: "suggest" });
+              break;
+            case "reject":
+              setMatchToSuggestDateTime({ match, reason: "reject" });
+              break;
             default:
               console.log("unhandled button-group-id:", detail.id);
           }
@@ -191,22 +349,14 @@ function InPlanningTable({ team: team_id, matches }) {
           team_id={team_id}
           match={matchToConfirmDateTime}
           onClose={() => setMatchToConfirmDateTime(undefined)}
-          onConfirm={() => {
-            suggestMatchTime(
-              {
-                suggesting_team: team_id,
-                match_id: matchToConfirmDateTime.id,
-                match_time: matchToConfirmDateTime.match_time,
-              },
-              {
-                onSuccess: () => {
-                  setMatchToConfirmDateTime(undefined);
-                  toast.success("Confirmed match time/date.");
-                },
-                onError: showErrorToast,
-              }
-            );
-          }}
+        />
+      )}
+      {matchToSuggestDateTime && (
+        <MatchTimeSuggestionModal
+          team_id={team_id}
+          match={matchToSuggestDateTime?.match}
+          reason={matchToSuggestDateTime?.reason}
+          onClose={() => setMatchToSuggestDateTime(undefined)}
         />
       )}
       <Table
